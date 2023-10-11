296611ddf84020344903c082f38388e84f39ce9d
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-10-11 11:23:54 +0100

    Added comments to the decryptedMessage function for clarity.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,3 @@
-/* 
-Las "llaves" de encriptación que utilizaremos son las siguientes:
-
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
-
-Requisitos:
-
-Debe funcionar solo con letras minúsculas
-No deben ser utilizados letras con acentos ni caracteres especiales
-Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
-Por ejemplo:
-"gato" => "gaitober"
-gaitober" => "gato"
-
-La página debe tener campos para
-inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
-El resultado debe ser mostrado en la pantalla.
-Extras:
-
-Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
- */
-
-/* Function to encrypt input and display the result */
-/* function encryptAndDisplayResult() {
-  // Capture the text from the input textarea
-  const inputTexarea = document.querySelector(".textarea-input");
-  const inputValue = inputTexarea.value;
-
-  // Encrypt the input text
-  const encrytedValue = encryptedMessage(inputValue);
-
-  // Display the encrypted text in the output textarea
-  const outputTextarea = document.querySelector(".textarea-output");
-  outputTextarea.value = encrytedValue;
-
-  // show display when the "Encrypt" button is clicked
-  var contenidoInicial = document.querySelector(".encriptador-output-inicial");
-  var contenidoEncriptado = document.querySelector(
-    ".resultado-encriptador-desencriptador"
-  );
-
-  contenidoInicial.style.display = "none";
-  contenidoEncriptado.style.display = "flex";
-} */
-
 function processText(encryptOrDecrypt) {
   // show display when the "Encrypt" button is clicked
   var contenidoInicial = document.querySelector(".encriptador-output-inicial");
@@ -102,9 +53,14 @@ function encryptedMessage(message) {
 // Function to decrypt a message based on the reverse substitutions.
 function decryptedMessage(message) {
   for (var key in substitutionVowels) {
+    // Create a regular expression to find the substitution and use the "g" flag to match all instances.
     var regex = new RegExp(substitutionVowels[key], "g");
+
+    // Replace all instances of the substitution with the original vowel using the regular expression.
     message = message.replace(regex, key);
   }
+
+  // Return the decrypted message with substitutions reversed.
   return message;
 }
 
